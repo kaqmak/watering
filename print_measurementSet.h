f296ddf87e4ad973690d8aeab1964e0199a7552d
kaqmak <github@kaqmak.dk>
2015-01-26 20:54:41 +0100

    Revert 4dbfef9..98f59c8
    This rolls back to commit 4dbfef90b52beff635a6d3d9913918f2a8743764.

--- print_measurementSet.h
+++
@@ -1,8 +0,0 @@
-void print_measurementSet(measurementSet dat) {
-  Serial.print("Time: ");
-  Serial.println(dat.time);
-  Serial.print("current_mA: ");
-  Serial.println(dat.current_mA);
-  Serial.print("loadvoltage: ");
-  Serial.println(dat.loadvoltage);
-}
